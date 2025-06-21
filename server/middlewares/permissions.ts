c62e68dfd4846f79457e0a15ae21970c689e76d5
arkarwebdev <arkar84848484@gmail.com>
2025-06-21 10:10:53 -0700

    shield update

--- server/middlewares/permissions.ts
+++ server/middlewares/permissions.ts
@@ -14,6 +14,18 @@ export const permissions = shield(
   {
     Query: {
       currentUser: isAuthenticated,
+      logout: isAuthenticated,
+    },
+    Mutation: {
+      createNewRoom: and(isAuthenticated, isAdmin),
+      updateRoom: and(isAuthenticated, isAdmin),
+      deleteRoom: and(isAuthenticated, isAdmin),
+
+      uploadAvatar: isAuthenticated,
+      updateUserProfile: isAuthenticated,
+      updateUserPassword: isAuthenticated,
+      forgetPassword: isAuthenticated,
+      resetPassword: isAuthenticated,
     },
   },
   { debug: true }
